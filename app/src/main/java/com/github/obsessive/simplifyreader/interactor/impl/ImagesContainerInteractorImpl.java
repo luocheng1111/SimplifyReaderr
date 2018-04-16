dc3500efdcecc6a6bfebb6e66e9b692879a0b015
luocc <1163011680@qq.com>
2018-04-16 17:37:18 +0800

    first commit

---
+++ app/src/main/java/com/github/obsessive/simplifyreader/interactor/impl/ImagesContainerInteractorImpl.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015 [1076559197@qq.com | tchen0707@gmail.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.obsessive.simplifyreader.interactor.impl;
+
+import android.content.Context;
+
+import com.github.obsessive.simplifyreader.R;
+import com.github.obsessive.simplifyreader.bean.BaseEntity;
+import com.github.obsessive.simplifyreader.interactor.CommonContainerInteractor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author:  Tau.Chen
+ * Email:   1076559197@qq.com | tauchen1990@gmail.com
+ * Date:    2015/4/2.
+ * Description:
+ */
+public class ImagesContainerInteractorImpl implements CommonContainerInteractor {
+    @Override
+    public List<BaseEntity> getCommonCategoryList(Context context) {
+        List<BaseEntity> resultData = new ArrayList<>();
+        String[] imagesCategoryArrayId = context.getResources().getStringArray(R.array.images_category_list_id);
+        String[] imagesCategoryArrayName = context.getResources().getStringArray(R.array.images_category_list_name);
+        resultData.add(new BaseEntity(imagesCategoryArrayId[0], imagesCategoryArrayName[0]));
+        resultData.add(new BaseEntity(imagesCategoryArrayId[1], imagesCategoryArrayName[1]));
+        resultData.add(new BaseEntity(imagesCategoryArrayId[2], imagesCategoryArrayName[2]));
+        resultData.add(new BaseEntity(imagesCategoryArrayId[3], imagesCategoryArrayName[3]));
+        resultData.add(new BaseEntity(imagesCategoryArrayId[4], imagesCategoryArrayName[4]));
+        resultData.add(new BaseEntity(imagesCategoryArrayId[5], imagesCategoryArrayName[5]));
+        return resultData;
+    }
+}
